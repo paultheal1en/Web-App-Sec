75638c0a6955ec2b0527efd8a1541d996848d764
Paul Tran <paultheal1en@gmail.com>
2024-05-21 18:54:18 +0700

    test

---
+++ README.md
@@ -0,0 +1,14 @@
+Sơ đồ trang Web nấu ăn.
+
+![image](https://github.com/DaoAnhThien/Web-NT208/assets/117579985/953cb414-d172-4c96-a7d6-68b58617cfa9)
+
+
+Các chức năng của trang Web:
+- Search công thức các món ăn.
+- Tìm kiếm các mẹo làm bếp bằng searchbox.
+- Đăng nhập để có thể lưu các công thức, mẹo bếp yêu thích.
+- Các trang menu chứa các món ăn được chia theo từng chủ đề.
+- Tạo công thức nấu ăn để làm các món ăn từ nguyên liệu qua hình ảnh có sẵn.
+- Góp ý, đóng góp thông qua form có sẵn từ trang web.
+  
+Link sheet góp ý: https://docs.google.com/spreadsheets/d/1Q7Ipr-ituq-qSm5cAGPOEovHy4n4FweQg5NsqBK3Pks/edit?usp=sharing
---
+++ src/Server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const path = require('path');
+const configViewEngine = require('./config/viewEngine');
+const webRouters = require('./routes/web');
+const apiRouters = require('./routes/api');
+const authRouters = require('./routes/auth');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+
+const app = express();
+const PORT = process.env.PORT || 8888;
+const HOST_NAME = process.env.HOST_NAME;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Cấu hình express-session
+app.use(session({
+    secret: process.env.SESSION_SECRET, // Chuỗi bí mật để ký và mã hóa cookie, có thể thay đổi
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+configViewEngine(app);
+
+// Routers
+app.use('/', webRouters);
+app.use('/', authRouters);
+app.use('/v1', apiRouters);
+
+modules.exports = app;
